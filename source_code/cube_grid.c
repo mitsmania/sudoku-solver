c1554afc7df3ad431564b564ba14cdd46f1717eb
Busa Srinivas <b25ee1010@iitj.ac.in>
2026-03-09 02:27:06 +0530

    added initialize function

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -1,4 +1,12 @@
-int main(){
-    int x = 2;
-    return 0;
+#include<stdio.h>
+int a[9][9][3];
+void initialize(){
+    int i, j ,k;
+    for(i=1; i<=9; i++){
+       for(j=1; j<=9; j++){
+          for(k=1; k<=3; k++){
+            a[i][j][k] = 0;
+          }
+       }
+    }
 }
