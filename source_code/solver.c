7c0e09990535cb03af4888175abc568ba1ac0472
Sourav Mallick <b25me1079@iitj.ac.in>
2026-03-10 01:29:35 +0530

    added function checkRow and checkCol

--- source_code/solver.c
+++ source_code/solver.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 
+int matrix[9][9][3];
+
+int checkRow(int layer, int row, int num){
+    for (int col=0; col<9; col++){
+        if (matrix[row][col][layer] == num){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int matrix[9][9][3];
+
+int checkCol(int layer, int col, int num){
+    for (int row=0; row<9; row++){
+        if (matrix[row][col][layer] == num){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
