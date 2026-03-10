58800fe521cb79efc431050f0fd6c08dcbead6e1
Sourav Mallick <b25me1079@iitj.ac.in>
2026-03-10 11:05:22 +0530

    Renamed function initialise to initMatrix, debugged initMatrix function

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-int a[9][9][3];
-void initialize(){
-    int i, j ,k;
-    for(i=1; i<=9; i++){
-       for(j=1; j<=9; j++){
-          for(k=1; k<=3; k++){
-            a[i][j][k] = 0;
+
+
+int matrix[9][9][3];
+
+
+void initMatrix(){
+    for(int row=0; row<9; row++){
+       for(int col=0; col<9; col++){
+          for(int layer=0; layer<3; layer++){
+            matrix[row][col][layer] = 0;
           }
        }
     }
