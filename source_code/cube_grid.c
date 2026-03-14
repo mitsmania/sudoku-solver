0485c1c01e6e69a6d61a75916f1a42287da7a17e
Zeba Md <b25cs1041@iitj.ac.in>
2026-03-14 23:11:11 +0530

    add get value

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -9,3 +9,9 @@ void initalizeGrid()
         for(int j=0;j<SIZE;j++)
             grid[9][9]=0;
 }
+void setvalue(int row,int col,int value){
+    grid[row][col]=value;
+}
+int getvalue(int row,int col){
+    return grid[row][col];
+}
