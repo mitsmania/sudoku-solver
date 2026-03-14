471969059af7542b485071078098ef34f1c4a6da
b25me1087-stack <b25me1087@iitj.ac.in>
2026-03-14 17:04:50 +0530

    initialize

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -1,52 +1,11 @@
 #include<stdio.h>
-int a[9][9][3];
-void initialize(){
-    int i, j ,k;
-    for(i=1; i<=9; i++){
-       for(j=1; j<=9; j++){
-          for(k=1; k<=3; k++){
-            a[i][j][k] = 0;
-          }
-       }
-    }
-}
+int SIZE;
 
-void printmatrix(){
-    int i, j, k;
-    for(k=1; k<=3; k++){
-        printf("layer: %d", k);
+int grid[9][9];
 
-        for(i=1; i<=9; i++){
-            for(j=1; j<=9; j++){
-                printf("%d", a[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-}
-
-void setvalue(int a[9][9][3], int i, int j, int k, int value){
-    if(i>=1 && i<=9 && j>=1 && j<=9 && k>=1 && k<=3){
-        a[i][j][k] = value;
-    }else{
-        printf("invalid coordinates\n");
-    }
-}
-
-void getValue(int a[9][9][3], int i, int j, int k, int value) {
-    if(i >= 1 && i <=9 && j >= 1 && j <=9 && k >= 1 && k <=3) {
-        return a[i][j][k];
-    } else {
-        printf("Invalid coordinates\n");
-        return -1;
-    }
-}
-
-void IscellEmpty( int a[9][9][3],int i, int j, int k ){
-    if (a[9][9][3] = 0){
-        return 1;
-    }else{
-        return 0;
-    } 
+void initalizeGrid()
+{
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            grid[9][9]=0;
 }
