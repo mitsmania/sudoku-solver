a093125ef3f20ccc1d13bb3a79624a7c40ea5f7d
Busa Srinivas <b25ee1010@iitj.ac.in>
2026-03-15 15:19:13 +0530

    added void function

--- source_code/cage.c
+++ source_code/cage.c
@@ -8,3 +8,19 @@ struct Cage{
     int cell_count;
     struct Cell cells[9];
 };
+void readCages(struct Cage cages[], int cage_count)
+{
+    for(int i = 0; i < cage_count; i++)
+    {
+        printf("\nCage %d\n", i+1);
+
+        printf("Enter sum: ");
+        scanf("%d", &cages[i].sum);
+
+        printf("Enter number of cells: ");
+        scanf("%d", &cages[i].cell_count);
+
+       
+    }
+}
+
