1a64ec34c1c390c05911bfb881452b6eb34cebcd
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-16 16:55:03 +0530

    added function declarations

--- source_code/grid.h
+++ source_code/grid.h
@@ -3,4 +3,10 @@
 
 #define SIZE 9
 
+void initalizeGrid(int grid[SIZE][SIZE]);
+void setvalue(int grid[SIZE][SIZE], int row,int col,int value);
+int getvalue(int grid[SIZE][SIZE], int row,int col);
+int isCellEmpty(int grid[SIZE][SIZE], int row,int col);
+void printGrid(int grid[SIZE][SIZE]);
+
 #endif
