a796ec990810428b1789bbd4bccd9516a1cd70e2
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-16 17:30:55 +0530

    included header file

--- source_code/cage.c
+++ source_code/cage.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
-struct Cell{
-    int row;
-    int col;
-};
-struct Cage{
-    int sum;
-    int cell_count;
-    struct Cell cells[9];
-};
+#include "cage.h"
 
 void readCages(struct Cage cages[], int cage_count)
 {
