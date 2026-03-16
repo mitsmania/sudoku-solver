d829a61a2d9cf418ac18c6f2691b0d49f95d7579
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-16 15:42:34 +0530

    Added grid as parameter for the functions

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -3,24 +3,24 @@ int SIZE = 9;
 
 int grid[9][9];
 
-void initalizeGrid()
+void initalizeGrid(int grid[SIZE][SIZE])
 {
     for(int i=0;i<SIZE;i++)
         for(int j=0;j<SIZE;j++)
             grid[9][9]=0;
 }
 
-void setvalue(int row,int col,int value)
+void setvalue(int grid[SIZE][SIZE], int row,int col,int value)
 {
     grid[row][col]=value;
 }
 
-int getvalue(int row,int col)
+int getvalue(int grid[SIZE][SIZE], int row,int col)
 {
     return grid[row][col];
 }
 
-int isCellEmpty(int row,int col) 
+int isCellEmpty(int grid[SIZE][SIZE], int row,int col) 
 {
     return grid[row][col]==0;
 }
