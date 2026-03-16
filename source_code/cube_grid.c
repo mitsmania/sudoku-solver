c738b2d173b9da44f556c0b3d5351e11b35b8489
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-16 15:38:36 +0530

    Added printGrid function

--- source_code/cube_grid.c
+++ source_code/cube_grid.c
@@ -9,15 +9,30 @@ void initalizeGrid()
         for(int j=0;j<SIZE;j++)
             grid[9][9]=0;
 }
+
 void setvalue(int row,int col,int value)
 {
     grid[row][col]=value;
 }
+
 int getvalue(int row,int col)
 {
     return grid[row][col];
 }
+
 int isCellEmpty(int row,int col) 
 {
     return grid[row][col]==0;
+}
+
+void printGrid(int grid[SIZE][SIZE])
+{
+    printf("\n");
+    for (int i=0; i<SIZE; i++){
+        for (int j=0; j<SIZE; j++){
+            printf("%d ",grid[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
