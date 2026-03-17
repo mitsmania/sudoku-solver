c0b8e6287d36d97e9e59b00b87f102f36bd09fab
b25me1087-stack <b25me1087@iitj.ac.in>
2026-03-17 21:46:05 +0530

    added colcheck function

--- source_code/validator.c
+++ source_code/validator.c
@@ -2,10 +2,19 @@
 
 int checkRow(int grid[9][9], int row, int num)
 {
-    for(int i= 0; i < 9; i++)//column varies
+    for(int i= 0; i < 9; i++)
     {
         if(grid[row][i] == num)
             return 0; // already exists
+    }
+    return 1;
+}
+int checkCol(int grid[9][9], int col, int num)
+{
+    for(int j = 0; j < 9; j++)
+    {
+        if(grid[j][col] == num)
+            return 0;//already exists
     }
     return 1;
 }
