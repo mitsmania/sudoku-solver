4d8bf7d1125fa4d6f6fc0bfd4e624e5c08965e2b
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-18 23:38:04 +0530

    created validator's header file

---
+++ source_code/validator.h
@@ -0,0 +1,10 @@
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+#include "grid.h"
+
+int checkRow(int grid[SIZE][SIZE], int row, int num);
+int checkCol(int grid[SIZE][SIZE], int col, int num);
+int checkBox(int grid[SIZE][SIZE], int row, int col, int num);
+int isValid(int grid[SIZE][SIZE], int row, int col, int num);
+
+#endif
