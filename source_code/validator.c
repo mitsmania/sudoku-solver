1447ad1a86328fc638f4d54475e2b02180d8f355
swechha pragna <b25ee1014@iitj.ac.in>
2026-03-18 21:41:28 +0530

    added box rule

--- source_code/validator.c
+++ source_code/validator.c
@@ -24,6 +24,16 @@ int checkBox(int grid[9][9], int row, int col, int num)
     int startRow = row - (row % 3);
     int startCol = col - (col % 3);
 
-    
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(grid[startRow + i][startCol + j] == num)
+            {
+                return 0; // number already exists in box
+            }
+        }
+    }
+
     return 1; 
 }
