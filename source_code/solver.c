b9247313223a22ccf5a9ac01d239b058c0b36e9d
sourav1079 <b25me1079@iitj.ac.in>
2026-03-21 01:22:35 +0530

    findEmptyCell function

--- source_code/solver.c
+++ source_code/solver.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int findEmptyCell (int grid[SIZE][SIZE], int *row, int *col){
+
+for (int i=0; i<SIZE; i++){
+for (int j=0; j<SIZE; j++){
+   if (isCellEmpty(grid, i, j) == 1){
+       *row = i;
+       *col = j;
+       return 1;
+   }
+}
+}
 return 0;
 }
