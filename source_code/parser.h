e34ded32fb09a0082c95510e8dea741ddae4078c
S Mithush Kumar <b25ph1012@iitj.ac.in>
2026-03-22 23:41:46 +0530

    completed header file for parser.c

--- source_code/parser.h
+++ source_code/parser.h
@@ -1,1 +1,12 @@
+#ifndef PARSER_H
+#define PARSER_H
 
+#include "grid.h"
+#include "cage.h"
+
+int parseFile(const char *filename,
+              int grid[SIZE][SIZE],
+              struct Cage cages[],
+              int *cage_count);
+
+#endif
