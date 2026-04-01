e8ca365f4b444b41443b95212ec549ad7279fb16
Busa Srinivas <b25ee1010@iitj.ac.in>
2026-04-01 20:57:38 +0530

    added structs

--- source_code/gui.c
+++ source_code/gui.c
@@ -8,3 +8,18 @@
 #include "cage_rules.h"
 #include "cage.h"
 
+#define GRID_SIZE 9
+#define CELL_SIZE 60
+#define OFFSET_X 50
+#define OFFSET_Y 50
+#define MAX_CAGES 100
+
+typedef struct {
+    int row, col;
+} Cell;
+
+typedef struct {
+    struct Cage cage; // backend cage
+    Color color;      
+} GUICage;
+
