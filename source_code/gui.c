c43b8222291831f0a7f0ae866e46230a8c607822
Busa Srinivas <b25ee1010@iitj.ac.in>
2026-04-01 20:56:28 +0530

    added headers

--- source_code/gui.c
+++ source_code/gui.c
@@ -1,1 +1,10 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>   // for atoi()
+#include "grid.h"
+#include "solver.h"
+#include "cage_rules.h"
+#include "cage.h"
 
