11d2f3d888a03ef55075479a3bb0fa05a0d95dc4
Zeba Md <b25cs1041@iitj.ac.in>
2026-04-10 22:47:59 +0530

    update

--- source_code/gui.c
+++ source_code/gui.c
@@ -49,3 +49,66 @@ int main() {
     // Edit mode for cage
     bool editingCage = false;
     int editingIndex = -1;
+ Bold 3x3 box borders
+        int lineWidth = 4;
+        for(int i=0;i<=GRID_SIZE;i+=3){
+            int x = OFFSET_X + i*CELL_SIZE;
+            DrawRectangle(x - lineWidth/2, OFFSET_Y, lineWidth, gridPixelSize, BLACK);
+
+            int y = OFFSET_Y + i*CELL_SIZE;
+            DrawRectangle(OFFSET_X, y - lineWidth/2, gridPixelSize, lineWidth, BLACK);
+        }
+
+        // Cage sums (top-left)
+        for(int k=0;k<cage_count;k++){
+            int topRow=100, leftCol=100;
+            for(int i=0;i<guiCages[k].cage.cell_count;i++){
+                int r=guiCages[k].cage.cells[i].row;
+                int c=guiCages[k].cage.cells[i].col;
+                if(r<topRow || (r==topRow && c<leftCol)){
+                    topRow=r;
+                    leftCol=c;
+                }
+            }
+            int x = OFFSET_X + leftCol*CELL_SIZE;
+            int y = OFFSET_Y + topRow*CELL_SIZE;
+            DrawText(TextFormat("%d", guiCages[k].cage.sum), x+2, y+2, 14, BLACK);
+        }
+
+        // Solution numbers
+        if(solved){
+            for(int i=0;i<GRID_SIZE;i++){
+                for(int j=0;j<GRID_SIZE;j++){
+                    if(solution[i][j]!=0){
+                        int x = OFFSET_X + j * CELL_SIZE;
+                        int y = OFFSET_Y + i * CELL_SIZE;
+                        DrawText(TextFormat("%d", solution[i][j]), x + CELL_SIZE/4, y + CELL_SIZE/4, 30, BLACK);
+                    }
+                }
+            }
+        }
+
+        // Show message if unsolvable
+        if(unsolvable){
+            DrawText("No solution found!", 200, 40, 35, RED);
+        }
+
+        // Sum input popup
+        if (enteringSum){
+            DrawRectangle(200, 250, 300, 150, LIGHTGRAY);
+            DrawRectangleLines(200, 250, 300, 150, BLACK);
+            DrawText("Enter Cage Sum:", 230, 270, 20, BLACK);
+            DrawRectangle(230, 310, 240, 30, WHITE);
+            DrawRectangleLines(230, 310, 240, 30, BLACK);
+            DrawText(sumInput, 240, 315, 20, BLACK);
+            DrawText("Press E to add more cells before confirming", 210, 360, 14, DARKGRAY);
+        }
+
+        DrawText("TAB: finalize | ENTER: confirm | ESC: cancel | S: solve", 50, 10, 20, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
