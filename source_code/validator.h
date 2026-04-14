4f6ae387b819ab85718ba9379ae678b90ac36ee2
sourav1079 <b25me1079@iitj.ac.in>
2026-04-14 21:56:41 +0530

    Declared isCageComplete

--- source_code/validator.h
+++ source_code/validator.h
@@ -6,5 +6,7 @@ int checkRow(int grid[SIZE][SIZE], int row, int num);
 int checkCol(int grid[SIZE][SIZE], int col, int num);
 int checkBox(int grid[SIZE][SIZE], int row, int col, int num);
 int isValid(int grid[SIZE][SIZE], int row, int col, int num);
+int isCageComplete(int grid[SIZE][SIZE], struct Cage cages[], int cage_count);
+
 
 #endif
