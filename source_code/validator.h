9cbf47994cad334b124ea52b65a660c387af271d
Sourav Mallick <b25me1079@iitj.ac.in>
2026-04-14 23:42:34 +0530

    Updated

--- source_code/validator.h
+++ source_code/validator.h
@@ -1,12 +1,13 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 #include "grid.h"
+#include "cage.h"
 
 int checkRow(int grid[SIZE][SIZE], int row, int num);
 int checkCol(int grid[SIZE][SIZE], int col, int num);
 int checkBox(int grid[SIZE][SIZE], int row, int col, int num);
 int isValid(int grid[SIZE][SIZE], int row, int col, int num);
-int isCageComplete(int grid[SIZE][SIZE], struct Cage cages[], int cage_count);
-
+int isCageComplete (int grid[SIZE][SIZE], struct Cage cages[], int cage_count);
+int isCageCompleteByIndex(int grid[SIZE][SIZE], struct Cage cages[], int cage_index);
 
 #endif
