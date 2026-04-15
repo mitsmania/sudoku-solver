6a2b3dd27fcbcd689dd9d12409f9e61229990188
Zeba Md <b25cs1041@iitj.ac.in>
2026-04-15 12:23:09 +0530

    update

---
+++ source_code/gui.c
@@ -0,0 +1,209 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>   // for atoi()
+#include "grid.h"
+#include "solver.h"
+#include "cage_rules.h"
+#include "cage.h"
+
+#define GRID_SIZE 9
+#define CELL_SIZE 60
+#define OFFSET_X 50
+#define OFFSET_Y 50
+#define MAX_CAGES 100
+
+typedef struct {
+    int row, col;
+} Cell;
+
+typedef struct {
+    struct Cage cage; // backend cage
+    Color color;      
+} GUICage;
+
+int main() {
+    InitWindow(700, 700, "Killer Sudoku - Cage Builder");
+    SetTargetFPS(60);
+
+    bool selected[GRID_SIZE][GRID_SIZE] = {false};
+    int cage_id[GRID_SIZE][GRID_SIZE] = {0};
+
+    GUICage guiCages[MAX_CAGES];
+    int cage_count = 0;
+
+    int solution[GRID_SIZE][GRID_SIZE] = {0};
+
+    bool isDrawing = false;
+    bool strokeModeAdd = true;
+    int lastRow = -1, lastCol = -1;
+
+    bool enteringSum = false;
+    char sumInput[10] = "";
+    int inputLen = 0;
+
+    bool solved = false;
+    bool unsolvable = false;
+
+    // Edit mode for cage
+    bool editingCage = false;
+    int editingIndex = -1;
+=======
+// Text input for sum
+        if (enteringSum) {
+            int key = GetCharPressed();
+            while (key > 0) {
+                if ((key >= '0' && key <= '9') && inputLen < 9) {
+                    sumInput[inputLen++] = (char)key;
+                    sumInput[inputLen] = '\0';
+                }
+                key = GetCharPressed();
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE) && inputLen > 0) {
+                inputLen--;
+                sumInput[inputLen] = '\0';
+            }
+
+            // Press E to add more cells before confirming
+            if (IsKeyPressed(KEY_E) && !editingCage && cage_count < MAX_CAGES) {
+                editingCage = true;
+                editingIndex = cage_count; // new cage index
+                // sumInput is kept as is
+                enteringSum = true; // reopen prompt
+            }
+
+            // Press ENTER to finalize cage
+            if (IsKeyPressed(KEY_ENTER) && inputLen > 0) {
+
+                struct Cage newCage;
+                newCage.cell_count = 0;
+                newCage.sum = atoi(sumInput);
+
+                for (int i = 0; i < GRID_SIZE; i++) {
+                    for (int j = 0; j < GRID_SIZE; j++) {
+                        if (selected[i][j]) {
+                            newCage.cells[newCage.cell_count].row = i;
+                            newCage.cells[newCage.cell_count].col = j;
+                            newCage.cell_count++;
+                        }
+                    }
+                }
+
+                if (newCage.cell_count > 0) {
+                    if (editingCage) {
+                        guiCages[editingIndex].cage = newCage;
+                        for (int i = 0; i < newCage.cell_count; i++) {
+                            int r = newCage.cells[i].row;
+                            int c = newCage.cells[i].col;
+                            cage_id[r][c] = editingIndex + 1;
+                            selected[r][c] = false;
+                        }
+                    } else {
+                        guiCages[cage_count].cage = newCage;
+                        guiCages[cage_count].color = (Color){
+                            GetRandomValue(100,255),
+                            GetRandomValue(100,255),
+                            GetRandomValue(100,255),
+                            255
+                        };
+                        for (int i = 0; i < newCage.cell_count; i++) {
+                            int r = newCage.cells[i].row;
+                            int c = newCage.cells[i].col;
+                            cage_id[r][c] = cage_count + 1;
+                            selected[r][c] = false;
+                        }
+                        cage_count++;
+                    }
+                }
+
+                enteringSum = false;
+                editingCage = false;
+                editingIndex = -1;
+            }
+
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                enteringSum = false;
+                editingCage = false;
+                editingIndex = -1;
+            }
+        }
+ while (!WindowShouldClose()) {
+
+        Vector2 mouse = GetMousePosition();
+        int col = (mouse.x - OFFSET_X) / CELL_SIZE;
+        int row = (mouse.y - OFFSET_Y) / CELL_SIZE;
+        bool insideGrid = (row >= 0 && row < GRID_SIZE && col >= 0 && col < GRID_SIZE);
+
+        // Cage drawing
+        if ((!enteringSum && !solved) || editingCage) {
+
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && insideGrid) {
+                isDrawing = true;
+                strokeModeAdd = !selected[row][col];
+            }
+
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+                isDrawing = false;
+                lastRow = -1;
+                lastCol = -1;
+            }
+
+            if (isDrawing && insideGrid) {
+                if ((row != lastRow || col != lastCol) &&
+                    (cage_id[row][col] == 0 || (editingCage && cage_id[row][col] == editingIndex + 1))) {
+                    selected[row][col] = strokeModeAdd;
+                    lastRow = row;
+                    lastCol = col;
+                }
+            }
+
+            
+            if (IsKeyPressed(KEY_TAB)) {
+                enteringSum = true;
+                if (!editingCage) {
+                    inputLen = 0;
+                    sumInput[0] = '\0';
+                }
+            }
+        }
+// 2nd person ends
+        // Text input for sum
+        if (enteringSum) {
+            int key = GetCharPressed();
+            while (key > 0) {
+                if ((key >= '0' && key <= '9') && inputLen < 9) {
+                    sumInput[inputLen++] = (char)key;
+                    sumInput[inputLen] = '\0';
+                }
+                key = GetCharPressed();
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE) && inputLen > 0) {
+                inputLen--;
+                sumInput[inputLen] = '\0';
+            }
+
+            // Press E to add more cells before confirming
+            if (IsKeyPressed(KEY_E) && !editingCage && cage_count < MAX_CAGES) {
+                editingCage = true;
+                editingIndex = cage_count; // new cage index
+                // sumInput is kept as is
+                enteringSum = true; // reopen prompt
+            }
+
+            // Press ENTER to finalize cage
+            if (IsKeyPressed(KEY_ENTER) && inputLen > 0) {
+
+                struct Cage newCage;
+                newCage.cell_count = 0;
+                newCage.sum = atoi(sumInput);
+                 DrawText("TAB: finalize | ENTER: confirm | ESC: cancel | S: solve", 50, 10, 20, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
